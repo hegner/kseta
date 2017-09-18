54d8715d926260801282df42e9978bc9f32ff500
David Butler <david.butler@kit.edu>
2017-09-18 15:27:29 +0200

    implemented inline lambda to replace incrementCounter

--- modernsyntax/lambda.cpp
+++ modernsyntax/lambda.cpp
@@ -13,7 +13,8 @@ void incrementCounter(int* counter, const unsigned int times){
 int main(){
   int counter(0);
   // launch a thread to increment the counter
-  std::thread increment(incrementCounter,&counter,100000);
+  
+  std::thread increment([](int* counter, const unsigned int times){for (unsigned int i=0; i<times;++i){++(*counter);}},&counter,100000);
   increment.join();
   std::cout << counter << std::endl;
   return 0;
